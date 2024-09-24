a26df4388218da080921f1cfa395948c6028e287
Nishant-Vats <nvats9690@gmail.com>
2024-09-24 12:35:18 +0530

    Random Emojies Added

---
+++ 21.Random-Emojies/app.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector(".emoji");
+
+const emojis = [
+     "😅",
+     "😆",
+     "😉",
+     "😊",
+     "😋",
+     "😎",
+     "🥲",
+     "😙",
+     "🥰",
+     "😘",
+     "😍",
+     "😚",
+     "☺️",
+     "🙂",
+     "🤗",
+     "🤩",
+     "🫥",
+     "😶",
+     "😑",
+     "😐",
+     "🤨",
+     "🫡"
+];
+
+btn.addEventListener("mouseover", ()=>{
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+});
