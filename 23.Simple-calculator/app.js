63ee1778656641ecd33b98546dd10cc781babb6f
Nishant-Vats <nvats9690@gmail.com>
2024-09-28 12:01:55 +0530

    Simple Calculator Added

---
+++ 23.Simple-calculator/app.js
@@ -0,0 +1,28 @@
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", ()=>{
+    let num1 = document.querySelector(".num1").value;
+    let num2 = document.querySelector(".num2").value;
+    let result = document.querySelector(".result");
+    let operator = document.getElementById("selectOp").value;
+   
+
+    switch(operator){
+        case "plus":
+            result.innerHTML = Number(num1) + Number(num2);
+            break;
+
+        case "min":
+            result.innerHTML = Number(num1) - Number(num2);
+            break;
+
+         case "div":
+            result.innerHTML = Number(num1) / Number(num2);
+            break;
+        
+        case "multi":
+            result.innerHTML = Number(num1) * Number(num2);
+            break;
+    }
+
+});
